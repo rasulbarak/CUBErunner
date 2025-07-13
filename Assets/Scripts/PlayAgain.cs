60b121c4c29be95cd41ff5d2fe8f2eb1c2a95bf8
rasulbarak <rasul.barak@sabanciuniv.edu>
2025-07-13 23:04:50 +0300

    add sound to PLAY AGAIN button on credits scene

--- Assets/Scripts/PlayAgain.cs
+++ Assets/Scripts/PlayAgain.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class PlayAgain : MonoBehaviour
 {
     public GameObject GetReadyUI;   // will reference the GetReady Panel
+    public AudioSource _audio;  // will control the audio
 
     public void Restart()
     // load the scene with build index 1, make method pubclic so it can be added as an on-click event
     {
-
+        _audio.enabled = true;  // play the sound
         GetReadyUI.SetActive(true);
 
         Invoke("GoToLevel1", 2);    // call the below function with a delay;
